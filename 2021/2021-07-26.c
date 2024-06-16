647660c87910ab9942dff60e88442d5ca3bb86dc
BerkeGulmen <150949932+BerkeGulmen@users.noreply.github.com>
2024-06-16 16:28:06 +0300

    Add files via upload

---
+++ 2021/2021-07-26.c
@@ -0,0 +1,24 @@
+/*
+BERKE GÜLMEN
+2021-07-26
+*/
+// Bu satırı makine diline çevirmez
+#include <stdio.h>
+main () {
+    int a;
+    a = 48;
+    int b;
+    b = 6;
+    int c;
+    c = a + b;
+    printf("Sayıların toplamı = %d", c);
+    printf("\n");
+    c = a - b;
+    printf("Sayıların farkı = %d", c);
+    printf("\n");
+    c = a * b;
+    printf("Sayıların çarpımı = %d", c);
+    c = a / b;
+    printf("\n");
+    printf("Sayıların oranı = %d", c);
+}
